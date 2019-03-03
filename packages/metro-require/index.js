d80e24b3c4030d0b8a79f6f87d109bcf917a5417
Alec Larson <alec.stanford.larson@gmail.com>
2019-03-03 11:37:43 -0500

    fixup: metro-require

--- packages/metro-require/index.js
+++ packages/metro-require/index.js
@@ -2,9 +2,6 @@ const Module = require('module');
 const path = require('path');
 const fs = require('fs');
 
-const loadModule = Module._load;
-Module._load = handleMetroPaths;
-
 const buildDir = '/build/';
 const srcPattern = /^([^\/]+)\/src\/(.+)$/;
 
@@ -13,26 +10,18 @@ const packageNames = fs.readdirSync(packagesDir);
 
 const cache = {};
 
+const resolve = Module._resolveFilename;
+Module._resolveFilename = (request, parent, isMain) =>
+  cache[request] || rewriteMetroPath(request, parent, isMain);
+
 // Paths to Metro modules are rewritten to the build directory.
-function handleMetroPaths(request, parent, isMain) {
-  let filename = cache[request];
-  if (filename) {
-    return loadModule(filename, parent, isMain);
-  }
-  if (request[0] !== '/') {
-    const sep = request.indexOf('/');
-    const pkg = sep == -1 ? request : request.slice(0, sep);
-    if (packageNames.indexOf(pkg) !== -1) {
-      request = packagesDir + request;
-    }
-  }
-  filename = Module._resolveFilename(request, parent, isMain);
+function rewriteMetroPath(request, parent, isMain) {
+  const filename = resolve(request, parent, isMain);
   if (filename.startsWith(packagesDir)) {
     const match = filename.slice(packagesDir.length).match(srcPattern);
     if (match) {
-      filename = packagesDir + match[1] + buildDir + match[2];
-      cache[arguments[0]] = filename;
+      return cache[request] = packagesDir + match[1] + buildDir + match[2];
     }
   }
-  return loadModule(filename, parent, isMain);
+  return filename;
 }
