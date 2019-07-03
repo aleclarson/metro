9058a6d7da26e5f84f46f8f2c043c057ec69b149
Alec Larson <alec.stanford.larson@gmail.com>
2019-07-03 10:50:30 -0400

    dev: better setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
+# Hook up our local Jest clone.
+lerna exec --scope=metro 'yarn link jest-worker jest-docblock jest-haste-map'
+lerna exec --scope=metro-core 'yarn link jest-haste-map'
+lerna exec --scope=metro-cache 'yarn link jest-serializer'
+
+# Install dependencies and link local packages together.
 lerna bootstrap
 
 # Run `yarn link` in each package.
-cd packages
-ls -1 | xargs -I {} sh -c 'cd {}; yarn link'
-
-# Hook up our local Jest clone.
-cd metro
-yarn link jest-worker jest-docblock jest-haste-map
-cd ../metro-core
-yarn link jest-haste-map
-cd ../metro-cache
-yarn link jest-serializer
+lerna exec 'yarn link' > /dev/null 2>&1
