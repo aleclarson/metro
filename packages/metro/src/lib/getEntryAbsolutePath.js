28bf000a8edc2baceede70ad8a565c554d7e9edd
Alec Larson <alec.stanford.larson@gmail.com>
2019-10-01 15:19:33 -0400

    fix: throw when an entry path does not exist

--- packages/metro/src/lib/getEntryAbsolutePath.js
+++ packages/metro/src/lib/getEntryAbsolutePath.js
@@ -12,14 +12,19 @@
 
 const getAbsolutePath = require('./getAbsolutePath');
 const path = require('path');
+const fs = require('fs-extra');
 
 import type {ConfigT} from 'metro-config/src/configTypes.flow';
 
 function getEntryAbsolutePath(config: ConfigT, entryFile: string): string {
-  if (config.projectRoot) {
-    return path.resolve(config.projectRoot, entryFile);
+  const result = config.projectRoot
+    ? path.resolve(config.projectRoot, entryFile)
+    : getAbsolutePath(entryFile, config.watchFolders);
+
+  if (!fs.existsSync(result)) {
+    throw new Error(`Entry path does not exist: "${result}"`);
   }
-  return getAbsolutePath(entryFile, config.watchFolders);
+  return result;
 }
 
 module.exports = getEntryAbsolutePath;
