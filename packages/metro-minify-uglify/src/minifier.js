3e792c6148d2ceb0ff6ecb628e08fa0b6a1b080b
Alec Larson <alec.stanford.larson@gmail.com>
2019-12-18 16:40:59 -0500

    fix: use terser instead of uglify-es

--- packages/metro-minify-uglify/src/minifier.js
+++ packages/metro-minify-uglify/src/minifier.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-const uglify = require('uglify-es');
+const terser = require('terser');
 
 import type {
   MetroMinifier,
@@ -46,7 +46,7 @@ function minify(
   inputMap: ?BabelSourceMap,
   options: MinifyOptions,
 ) {
-  const result = uglify.minify(inputCode, {
+  const result = terser.minify(inputCode, {
     mangle: {
       toplevel: false,
       reserved: options.reserved,
