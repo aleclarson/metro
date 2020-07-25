997ab79811b190a9ad74b66fd9ba23525c36992c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-07-25 16:50:48 -0400

    fix(metro-require): pass along the `options` argument in Module._resolveFilename

--- packages/metro-require/index.js
+++ packages/metro-require/index.js
@@ -9,12 +9,12 @@ const packagesDir = path.resolve(__dirname, '..') + '/';
 const cache = {};
 
 const resolve = Module._resolveFilename;
-Module._resolveFilename = (request, parent, isMain) =>
-  cache[request] || rewriteMetroPath(request, parent, isMain);
+Module._resolveFilename = (request, parent, isMain, options) =>
+  cache[request] || rewriteMetroPath(request, parent, isMain, options);
 
 // Paths to Metro modules are rewritten to the build directory.
-function rewriteMetroPath(request, parent, isMain) {
-  const filename = resolve(request, parent, isMain);
+function rewriteMetroPath(request, parent, isMain, options) {
+  const filename = resolve(request, parent, isMain, options);
   if (filename.startsWith(packagesDir)) {
     const match = filename.slice(packagesDir.length).match(srcPattern);
     if (match) {
